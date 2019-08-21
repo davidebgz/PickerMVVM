c771f449da37743aff5aa6e5c91ebfc16843edfc
David bolaños <davidebgz@hotmail.com>
2019-08-21 17:32:08 -0600

    nuevoy change

--- PickerMVVM/PickerMVVM/ViewModel/PickerViewModel.cs
+++ PickerMVVM/PickerMVVM/ViewModel/PickerViewModel.cs
@@ -1,20 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace PickerMVVM.ViewModel
 {
-   public class PickerViewModel
+   public class PickerViewModel : INotifyPropertyChanged
     {
         public List<ciudades> ListaCiudades { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertchanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private ciudades _selecionarCiudad;
+
+        public ciudades SelecionarCiudad
+        {
+            get { return _selecionarCiudad; }
+            set
+            {
+                     if (_selecionarCiudad != value)
+                     {
+                         _selecionarCiudad = value;
+
+                    miciudad = "Ciudad Selecionada :" + _selecionarCiudad.valor;  
+
+                     }
+            }
+        }
+        private string _miCiudad;
+
+        public string miciudad
+        {
+            get { return _miCiudad; }
+            set
+            {
+                if (_miCiudad != value)
+                {
+                    _miCiudad = value;
+                    OnPropertchanged();
+                }
+            }
+        }
+
+
         public PickerViewModel()
         {
             ListaCiudades = GetCiudades().OrderBy(t => t.valor).ToList();
         }
+
+       
+
         public List<ciudades> GetCiudades()
         {
-            var ciudades = new List<ciudades>()
+            var ciudad = new List<ciudades>()
             {
                 new ciudades() {llave = 1, valor= "Mumbai"},
                 new ciudades() {llave = 2, valor= "Dubai"},
@@ -23,7 +66,7 @@ namespace PickerMVVM.ViewModel
                 new ciudades() {llave = 5, valor= "Berlin"},
                 new ciudades() {llave = 6, valor= "SanSalvador"},
             };
-            return ciudades;
+            return ciudad;
         }
     }
     public class ciudades
